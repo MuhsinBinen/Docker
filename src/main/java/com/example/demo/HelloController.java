89b44600707aa5575446f9a70a47ddd4f8d1356c
Muhsin BİNEN <99399662+MuhsinBinen@users.noreply.github.com>
2024-07-02 23:11:37 +0300

    update7

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
         @GetMapping("/hello")
         public String hello() {
-            return "Hello, Worldtahirtolutahirrrrrerar!";
+            return "Hello world!";
         }
 
     }
